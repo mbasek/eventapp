411466ce3abd3240239f90c34e5489a697da7793
Marko Basek <mbbasek@hotmail.com>
2024-01-06 15:56:35 +0100

    login/register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/HomePage";
+import Login from "./pages/LoginPage";
+import Register from "./pages/RegisterPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <Home />
+      <Routes>
+        <Route path="/" Component={Home} exact />
+        <Route path="/login" Component={Login} exact />
+        <Route path="/register" Component={Register} exact />
+      </Routes>
     </BrowserRouter>
   );
 }
